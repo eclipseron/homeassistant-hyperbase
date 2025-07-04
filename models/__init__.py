c8a28efe06a9d8a80da33de7e4cea1cbe0d10632
PetrusAriaa <petrusariacr25@gmail.com>
2025-07-04 20:37:55 +0700

    feat: create schema for light

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ from homeassistant.core import State
 from homeassistant.helpers.device_registry import DeviceEntry
 from homeassistant.helpers.entity_registry import RegistryEntry
 from .binary_sensor import BinarySensorColumns, BinarySensorEntityData, BinarySensorModel, BINARY_SENSOR_COLUMNS
-from .light import LIGHT_COLUMNS, LightModel
+from .light import LIGHT_COLUMNS, LightEntityData, LightModel, LightColumns
 from .sensor import SENSOR_COLUMNS, SensorEntityData, SensorModel, SensorColumns
 from .switch import SWITCH_COLUMNS, SwitchColumns, SwitchEntityData, SwitchModel
 from .base import BASE_COLUMNS, BaseModel
@@ -40,6 +40,10 @@ def create_schema(entity_domains: list[DomainDeviceClass]) -> dict[str, dict[str
                 _additional_cols = SwitchColumns(entity_domain.device_clasess)
                 schema = {**schema, **_additional_cols.schema}
                 continue
+            case Platform.LIGHT:
+                _additional_cols = LightColumns()
+                schema = {**schema, **_additional_cols.schema}
+                continue
             case _:
                 schema = {
                     **schema,
@@ -49,52 +53,20 @@ def create_schema(entity_domains: list[DomainDeviceClass]) -> dict[str, dict[str
     return schema
 
 
-def parse_entity_data(entity_entry: RegistryEntry, value: Any | None = None):
+def parse_entity_data(entity_entry: RegistryEntry, state: State):
     match entity_entry.domain:
         case Platform.SENSOR:
             device_class = entity_entry.original_device_class
-            sensor = SensorEntityData(device_class, value)
+            sensor = SensorEntityData(device_class, state.state)
             return sensor.data
         case Platform.BINARY_SENSOR:
             device_class = entity_entry.original_device_class
-            binary_sensor = BinarySensorEntityData(device_class, value)
+            binary_sensor = BinarySensorEntityData(device_class, state.state)
             return binary_sensor.data
         case Platform.SWITCH:
             device_class = entity_entry.original_device_class
-            switch = SwitchEntityData(device_class, value)
+            switch = SwitchEntityData(device_class, state.state)
             return switch.data
-        case _:
-            pass
-
-
-def parse_data(connector_serial_number: str,
-            connector_entity: str,
-            device_entry: DeviceEntry,
-            entity_entry: RegistryEntry,
-            state: State,
-            last_reported: datetime):
-    """parse data """
-    
-    base_info = BaseModel(
-        connector_serial_number=connector_serial_number,
-        connector_entity=connector_entity,
-        product_id=device_entry.dict_repr["identifiers"][0][1],
-        name_default=device_entry.name,
-        name_by_user=device_entry.name_by_user,
-        area_id=device_entry.area_id,
-    )
-    match entity_entry.domain:
-        case Platform.SENSOR:
-            sensor = SensorModel(
-                base_info,
-                device_class=entity_entry.original_device_class,
-                last_reset = last_reported,
-                unit_of_measurement=entity_entry.unit_of_measurement,
-                value=state.state,
-                state_class=entity_entry.capabilities.get("state_class", None)
-            )
-            return sensor.data
-
         case Platform.LIGHT:
             hs_color = state.attributes.get("hs_color", None)
             rgb_color = state.attributes.get("rgb_color", None)
@@ -121,33 +93,105 @@ def parse_data(connector_serial_number: str,
                 x_color = xy_color[0]
                 y_color = xy_color[1]
             
-            light = LightModel(
-                base_info,
+            light = LightEntityData(
+                state_value=state.state,
                 brightness=state.attributes.get("brightness", None),
-                color_mode=state.attributes.get("color_mode", None),
                 color_temp_kelvin=state.attributes.get("color_temp_kelvin", None),
-                effect=state.attributes.get("effect", None),
                 hue=hue,
                 saturation=saturation,
-                is_on=state.state == "on",
-                max_color_temp_kelvin=state.attributes.get("max_color_temp_kelvin", None),
-                min_color_temp_kelvin=state.attributes.get("min_color_temp_kelvin", None),
                 color_temp=state.attributes.get("color_temp", None),
                 red=red,
                 green=green,
                 blue=blue,
                 x_color=x_color,
-                y_color=y_color,
+                y_color=y_color
             )
             return light.data
-        
-        
-        case Platform.BINARY_SENSOR:
-            binary_sensor = BinarySensorModel(
-                base_info,
-                device_class=entity_entry.original_device_class,
-                is_on=state.state == "on"
-            )
-            return binary_sensor.data
         case _:
-            return None
+            pass
+
+
+# def parse_data(connector_serial_number: str,
+#             connector_entity: str,
+#             device_entry: DeviceEntry,
+#             entity_entry: RegistryEntry,
+#             state: State,
+#             last_reported: datetime):
+#     """parse data """
+    
+#     base_info = BaseModel(
+#         connector_serial_number=connector_serial_number,
+#         connector_entity=connector_entity,
+#         product_id=device_entry.dict_repr["identifiers"][0][1],
+#         name_default=device_entry.name,
+#         name_by_user=device_entry.name_by_user,
+#         area_id=device_entry.area_id,
+#     )
+#     match entity_entry.domain:
+#         case Platform.SENSOR:
+#             sensor = SensorModel(
+#                 base_info,
+#                 device_class=entity_entry.original_device_class,
+#                 last_reset = last_reported,
+#                 unit_of_measurement=entity_entry.unit_of_measurement,
+#                 value=state.state,
+#                 state_class=entity_entry.capabilities.get("state_class", None)
+#             )
+#             return sensor.data
+
+#         case Platform.LIGHT:
+#             hs_color = state.attributes.get("hs_color", None)
+#             rgb_color = state.attributes.get("rgb_color", None)
+#             xy_color = state.attributes.get("xy_color", None)
+            
+#             hue = None
+#             saturation = None
+#             red = None
+#             green = None
+#             blue = None
+#             x_color = None
+#             y_color = None
+            
+#             if hs_color is not None:
+#                 hue = hs_color[0]
+#                 saturation = hs_color[1]
+            
+#             if rgb_color is not None:
+#                 red = rgb_color[0]
+#                 green = rgb_color[1]
+#                 blue = rgb_color[2]
+            
+#             if xy_color is not None:
+#                 x_color = xy_color[0]
+#                 y_color = xy_color[1]
+            
+#             light = LightModel(
+#                 base_info,
+#                 brightness=state.attributes.get("brightness", None),
+#                 color_mode=state.attributes.get("color_mode", None),
+#                 color_temp_kelvin=state.attributes.get("color_temp_kelvin", None),
+#                 effect=state.attributes.get("effect", None),
+#                 hue=hue,
+#                 saturation=saturation,
+#                 is_on=state.state == "on",
+#                 max_color_temp_kelvin=state.attributes.get("max_color_temp_kelvin", None),
+#                 min_color_temp_kelvin=state.attributes.get("min_color_temp_kelvin", None),
+#                 color_temp=state.attributes.get("color_temp", None),
+#                 red=red,
+#                 green=green,
+#                 blue=blue,
+#                 x_color=x_color,
+#                 y_color=y_color,
+#             )
+#             return light.data
+        
+        
+#         case Platform.BINARY_SENSOR:
+#             binary_sensor = BinarySensorModel(
+#                 base_info,
+#                 device_class=entity_entry.original_device_class,
+#                 is_on=state.state == "on"
+#             )
+#             return binary_sensor.data
+#         case _:
+#             return None
--- models/light.py
+++ models/light.py
@@ -24,6 +24,71 @@ LIGHT_COLUMNS = {
     "y_color": {"kind": "int", "required": False},
 }
 
+
+class LightColumns:
+    def __init__(self):
+        self.__columns = {
+            "light_brightness": {"kind": "int", "required": False},
+            "light_color_temp_kelvin": {"kind": "int", "required": False},
+            "light_hue": {"kind": "double", "required": False},
+            "light_saturation": {"kind": "double", "required": False},
+            "light_is_on": {"kind": "boolean", "required": False},
+            "light_color_temp": {"kind": "int", "required": False},
+            "light_red": {"kind": "int", "required": False},
+            "light_green": {"kind": "int", "required": False},
+            "light_blue": {"kind": "int", "required": False},
+            "light_x_color": {"kind": "double", "required": False},
+            "light_y_color": {"kind": "double", "required": False},
+        }
+    
+    @property
+    def schema(self):
+        return self.__columns
+
+
+class LightEntityData:
+    def __init__(self,
+        state_value: Any | None = None,
+        brightness: int | None = None,
+        color_temp_kelvin: int | None = None,
+        hue: float | None = None,
+        saturation: float | None = None,
+        color_temp: int | None = None,
+        red: int | None = None,
+        green: int | None = None,
+        blue: int | None = None,
+        x_color: float | None = None,
+        y_color: float | None = None
+        ):
+        self.__state_value = state_value
+        self.__brightness = brightness
+        self.__color_temp_kelvin = color_temp_kelvin
+        self.__hue = hue
+        self.__saturation = saturation
+        self.__color_temp = color_temp
+        self.__red = red
+        self.__green = green
+        self.__blue = blue
+        self.__x_color = x_color
+        self.__y_color = y_color
+    
+    @property
+    def data(self):
+        return {
+            "light_brightness": self.__brightness,
+            "light_color_temp_kelvin": self.__color_temp_kelvin,
+            "light_hue": self.__hue,
+            "light_saturation": self.__saturation,
+            "light_is_on": self.__state_value == "on",
+            "light_color_temp": self.__color_temp, 
+            "light_red": self.__red,
+            "light_green": self.__green,
+            "light_blue": self.__blue,
+            "light_x_color": self.__x_color,
+            "light_y_color": self.__y_color,
+        }
+
+
 class LightModel():
     """asdad"""
     def __init__(self,
